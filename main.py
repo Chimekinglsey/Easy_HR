42e111378fb89c189c8cc084f0ee78fcc5419690
Nwamanna <nwamannazika@gmail.com>
2023-09-01 00:07:59 +0100

    fix employee table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
-from .model_user import User
+from .model_user import User, Employee
 from . import db
 
 main = Blueprint('main', __name__)
@@ -22,3 +22,35 @@ def dashboard():
 @login_required
 def manage_emp():
     return render_template('manage_employee.html')
+
+@main.route('user/add_employee', methods=['GET', 'POST'])
+@login_required
+def add_emp():
+    if request.method is 'POST':
+        firstName = request.form.get("firstName") 
+        middleName = request.form.get("middleName") 
+        lastName = request.form.get("lastName")
+        email = request.form.get("email")
+        phoneNumber = request.form.get("phoneNumber") 
+        address = request.form.get("address") 
+        nationality = request.form.get("nationality") 
+        stateOfOrigin = request.form.get("stateOfOrigin") 
+        employeeID = request.form.get("employeeID") 
+        level = request.form.get("level") 
+        salary = request.form.get("salary") 
+        branch = request.form.get("branch") 
+        department = request.form.get("department") 
+        DateOfEmployment = request.form.get("DateOfEmployment") 
+        dateOfBirth = request.form.get("dateOfBirth") 
+        gender = request.form.get("gender")
+
+        new_employee = Employee(firstName=firstName, middleName=middleName,
+                                lastName=lastName, email=email, phoneNumber=phoneNumber,
+                                address=address, nationality=nationality, stateOfOrigin=stateOfOrigin,
+                                employeeID=employeeID, level=level, salary=salary,
+                                branch=branch, department=department, DateOfEmployment=DateOfEmployment,
+                                dateOfBirth=dateOfBirth, gender=gender)
+        db.session.add(new_employee)
+        db.session.commit()
+        return redirect(url_for('main.manage_emp'))
+    return url_for('main.manage_emp')
--- model_user.py
+++ model_user.py
@@ -1,11 +1,12 @@
 from flask_login import UserMixin
-from datetime import date
+from datetime import date, datetime
 from . import db
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    first_name = db.Column(db.String(300))
-    last_name = db.Column(db.String(300))
+    first_name = db.Column(db.String(30))
+    last_name = db.Column(db.String(30))
     DOB = db.Column(db.Date, default=date.today)
     company_name = db.Column(db.String(300))
     official_email = db.Column(db.String(100), unique=True)
@@ -13,4 +14,22 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(100))
 
 class Employee(db.Model):
-    """ """
+    id = db.Column(db.Integer, primary_key=True) #
+    firstName = db.Column(db.String(30), nullable=False)
+    middleName = db.Column(db.String(30), nullable=False)
+    lastName = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    phoneNumber = db.Column(db.String(15), nullable=False, unqiue=True)
+    address = db.Column(db.String(100), nullable=False)
+    nationality = db.Column(db.String(30), nullable=False)
+    stateOfOrigin = db.Column(db.String(30), nullable=False)
+    employeeID = db.Column(db.String(30), nullable=False, unqiue=True)
+    level = db.Column(db.Enum('Entry', 'Junior', 'Mid', 'Senior', 'Executive'), nullable=False)
+    salary = db.Column(db.Integer, nullable=False)
+    branch = db.Column(db.String(30), nullable=False)
+    department = db.Column(db.String(30), nullable=False)
+    DateOfEmployment = db.Column(db.Date, nullable=False, default=datetime)
+    dateOfBirth = db.Column(db.Date, nullable=False, default=datetime)
+    gender = db.Column(db.Enum('Female', 'Male'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
