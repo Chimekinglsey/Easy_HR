60152d0472d46e6bf60b069570987a7914886b63
chimekinglsey <onlinekingsley@gmail.com>
2023-09-05 23:18:37 +0100

    working on flask messages

--- static/assets/scripts/default.js
+++ static/assets/scripts/default.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     setTimeout(function() {
-        const notification = document.querySelector('.flash_messages');
+        const notification = document.querySelector('.red_background');
         if (notification) {
             notification.classList.add('fade-out');
         }
-    }, 4000);
+        notification.addEventListener.style.display = 'none';
+    }, 3000)
+    ;
 })
